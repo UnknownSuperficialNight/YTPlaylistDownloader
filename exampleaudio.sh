28b14b325542e1a0d4a332e6d5096fb84cbe6717
UnknownSuperficialNight <88142731+UnknownSuperficialNight@users.noreply.github.com>
2022-05-31 05:53:41 +1200

    Add files via upload

---
+++ exampleaudio.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# you can replace wget or ignore it if u want this is just for a quick start
+wget https://github.com/UnknownSuperficialNight/YTPlaylistDownloader/raw/main/ytconfig.txt
+echo browser_name_here > cookies.txt
+echo > embeded
+read -p "press enter if u want to download the autodownloadclient (optional)"
+wget https://github.com/UnknownSuperficialNight/YTPlaylistDownloader/raw/main/autodownloadclient.sh.x
